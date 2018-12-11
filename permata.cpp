6f0a3f445cf459c937b2bad488cf500bf37e55e6
Alvin Meko <oscar-alvin@users.noreply.github.com>
2018-12-11 13:59:11 +0700

    Add files via upload

---
+++ permata.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main () {
+
+    int n, a, b;
+    printf ("Masukkan Batasan Angka : ");scanf ("%d", &n);
+    printf ("\n");
+    for (a = 1; a <= n; a++) {
+        for (b = n; b >= a; b--) {
+            printf (" ");
+        }
+        for (b = 1; b <= a; b++) {
+            printf ("*");
+        }
+        for (b = (a-1); b >= 1; b--) {
+            printf ("*");
+        }
+        printf ("\n");
+    }
+
+    for (a = 1; a <= n-1; a++) {
+        for (b = 0; b <= a; b++) {
+            printf (" ");
+        }
+        for (b = 1; b <= (n-a); b++) {
+            printf ("*");
+        }
+        for (b = (n-a-1); b >= 1; b--) {
+            printf ("*");
+        }
+        printf ("\n");
+    }
+    return 0;
+}
